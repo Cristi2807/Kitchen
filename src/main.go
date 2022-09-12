6b1f3f3545b74e9fd276fd4bc6b4d86661223690
Cristi <cristian.boris@isa.utm.md>
2022-09-12 10:44:18 +0300

    Add docker container

--- src/main.go
+++ src/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getOrder(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
 
 func main() {
-	fmt.Printf("Starting server at port 8080\n")
+	http.HandleFunc("/", getOrder)
+	err := http.ListenAndServe(":8010", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed \n")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s \n", err)
+		os.Exit(1)
+	}
 }
